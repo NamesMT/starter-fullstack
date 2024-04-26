eb52f75a5e0967d8a2598b58f4bc536eb54f157f
NamesMT <dangquoctrung123@gmail.com>
2024-04-26 15:41:31 +0000

    chore: update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -14,19 +14,19 @@ export default frontendNuxtConfig(antfu(
       '**/assets/vendor/**',
     ],
   },
-  // Allow trailing space for markdown formatting
-  {
-    files: ['*.md'],
-    rules: {
-      'style/no-trailing-spaces': 'off',
-    },
-  },
   {
     rules: {
       // Allow trailing space in comments, for possible JSDoc formattings
       'style/no-trailing-spaces': ['error', { ignoreComments: true }],
       // Relaxes inline statements a bit
       'style/max-statements-per-line': ['error', { max: 2 }],
+    },
+  },
+  // Allow trailing space for markdown formatting
+  {
+    files: ['**/*.md'],
+    rules: {
+      'style/no-trailing-spaces': 'off',
     },
   },
 ))
