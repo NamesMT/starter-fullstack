357db6c55ed6ffe31c78fd25c0eb5241ae051455
NamesMT <dangquoctrung123@gmail.com>
2024-04-30 03:13:41 +0000

    chore(sst): lower the memory configuration,

--- sst.config.ts
+++ sst.config.ts
@@ -12,6 +12,9 @@ export default $config({
     const backend = new sst.aws.Function('Backend', {
       url: true,
       handler: 'apps/backend/src/index.handler',
+      timeout: '60 seconds',
+      // If you need to process a big amount of data, you should create sub "job" functions instead of rising the the spec of the main function
+      memory: '300 MB',
       streaming: true,
     })
 
