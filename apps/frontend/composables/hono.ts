9f0c0851bddbff5120004862a74ec098af35d15d
NamesMT <dangquoctrung123@gmail.com>
2024-04-30 14:39:09 +0000

    feat(hcRes)!: should throw if `!ok`

--- apps/frontend/composables/hono.ts
+++ apps/frontend/composables/hono.ts
@@ -4,7 +4,9 @@ import type { ClientResponse } from 'hono/client'
 type GetInnerType<S> = S extends ClientResponse<infer T> ? T : never
 export async function hcRes<T extends ClientResponse<any>>(fetchRes: Awaitable<T>): Promise<GetInnerType<T> extends Record<string, never> ? unknown : GetInnerType<T>> {
   const res = await fetchRes
+  // @ts-expect-error res incompatible
+  const parsedRes = parseResponse(res)
 
-  // @ts-expect-error res incompatible, parseResponse unknown
-  return parseResponse(res)
+  // @ts-expect-error unknown not assignable
+  return res.ok ? parsedRes : Promise.reject(parsedRes)
 }
