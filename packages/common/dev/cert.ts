cffb545757215641103f293aead37a294e4b56dd
NamesMT <dangquoctrung123@gmail.com>
2024-05-03 09:22:39 +0000

    refactor: DRY-er

--- packages/common/dev/cert.ts
+++ packages/common/dev/cert.ts
@@ -2,7 +2,8 @@ import { fileURLToPath } from 'node:url'
 import { readFileSync } from 'node:fs'
 import { dirname, resolve } from 'pathe'
 
-export const localcertPath = resolve(dirname(fileURLToPath(import.meta.url)), `localcert.pem`)
-export const localcertKeyPath = resolve(dirname(fileURLToPath(import.meta.url)), `localcert-key.pem`)
+const currentDir = dirname(fileURLToPath(import.meta.url))
+export const localcertPath = resolve(currentDir, `localcert.pem`)
+export const localcertKeyPath = resolve(currentDir, `localcert-key.pem`)
 export const localcert = readFileSync(localcertPath)
 export const localcertKey = readFileSync(localcertKeyPath)
