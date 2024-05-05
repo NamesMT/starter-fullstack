ecf977894138fd18a8cbc1606c7403c389f1d24e
NamesMT <dangquoctrung123@gmail.com>
2024-05-05 16:10:59 +0000

    chore: simplify rpc exports, tsconfig

--- apps/frontend/plugins/01.rpcApi.ts
+++ apps/frontend/plugins/01.rpcApi.ts
@@ -1,12 +1,12 @@
 import { hc } from 'hono/client'
-import type { AppType } from 'backend/rpc'
+import type { app } from 'backend'
 
 export default defineNuxtPlugin(async () => {
   const url = useRequestURL()
   // If the frontend and backend domain are on the same domain, we will call the proxy instead of the backendUrl directly
   const backendUrl = useRuntimeConfig().public.backendUrl
   const urlBackend = new URL(backendUrl)
-  const apiClient = hc<AppType>(
+  const apiClient = hc<typeof app>(
     urlBackend.hostname === url.hostname
       ? `https://${url.host}`
       : backendUrl,
