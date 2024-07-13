a617e54b177f281db3916387eaf7f680a0bc8a69
NamesMT <dangquoctrung123@gmail.com>
2024-07-13 04:42:55 +0000

    feat(frontend): move shadcn's `cn` util to app level

--- apps/frontend/app/utils/css.ts
+++ apps/frontend/app/utils/css.ts
@@ -1,2 +1,9 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export { cssUnit } from '@local/common'
 export type { LooseCSSUnit } from '@local/common'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
