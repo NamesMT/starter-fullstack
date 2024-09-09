c0520cb7e83ca04774cc5c35dc42e0119433a4b1
NamesMT <dangquoctrung123@gmail.com>
2024-09-09 09:42:07 +0000

    chore(vue-query): set `gcTime` to `maxAge`

--- apps/frontend/app/vue-query.config.ts
+++ apps/frontend/app/vue-query.config.ts
@@ -16,21 +16,24 @@ function newIdbStorage(idbStore: UseStore): AsyncStorage<PersistedQuery> {
 export default defineVueQueryPluginHook(({ queryClient }) => {
   const maxAge = 1000 * 60 * 60 * 12 // 12 hours
 
-  queryClient.setDefaultOptions({ queries: {
-    // Only enable Tanstack Query on client-side
-    enabled: import.meta.client,
-    // Set default staleTime to Infinity to manually invalidate the query only when needed
-    staleTime: Infinity,
-    // Set client-side persister to IndexedDB
-    persister: !import.meta.client
-      ? undefined
-      : experimental_createPersister<PersistedQuery>({
-        storage: newIdbStorage(createStore('tsq_db', 'tsq_store')),
-        maxAge,
-        serialize: persistedQuery => persistedQuery,
-        deserialize: cached => cached,
-      }),
-  } })
+  queryClient.setDefaultOptions({
+    queries: {
+      gcTime: maxAge,
+      // Only enable Tanstack Query on client-side
+      enabled: import.meta.client,
+      // Set default staleTime to Infinity to manually invalidate the query only when needed
+      staleTime: Infinity,
+      // Set client-side persister to IndexedDB
+      persister: !import.meta.client
+        ? undefined
+        : experimental_createPersister<PersistedQuery>({
+          storage: newIdbStorage(createStore('tsq_db', 'tsq_store')),
+          maxAge,
+          serialize: persistedQuery => persistedQuery,
+          deserialize: cached => cached,
+        }),
+    },
+  })
 
-  return { pluginReturn: { }, vueQueryPluginOptions: { } }
+  return { pluginReturn: {}, vueQueryPluginOptions: {} }
 })
