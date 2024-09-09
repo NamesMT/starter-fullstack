5099915d867d7d38087b2e6ba873e0f2af0db194
NamesMT <dangquoctrung123@gmail.com>
2024-09-09 03:24:47 +0000

    refactor: Remove hack, use official `tsx watch --include`

--- packages/locales/index.ts
+++ packages/locales/index.ts
@@ -1,9 +1,5 @@
 import { createContext } from 'unplugin-sheet-i18n'
 
-// Hacky workaround to reload i18n.csv when it changes, waiting for https://github.com/privatenumber/tsx/issues/181
-// @ts-expect-error it'll throw, but it's ok, we just want to hint tsx to relaod upon i18n.csv change
-import('./src/i18n.csv').catch(() => null)
-
 createContext({
   outDir: 'dist',
 }).scanConvert()
