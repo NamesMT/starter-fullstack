2804d63ea3bbfe29942e2f0d775657215c114744
NamesMT <dangquoctrung123@gmail.com>
2024-09-09 04:46:17 +0000

    fix: should stringify ArkErrors for full logging

--- apps/backend/src/middlewares/arktype.ts
+++ apps/backend/src/middlewares/arktype.ts
@@ -5,6 +5,6 @@ type arktypeValidatorArguments = Parameters<typeof arktypeValidator>
 export function customArktypeValidator<Target extends arktypeValidatorArguments[0], Schema extends arktypeValidatorArguments[1]>(target: Target, schema: Schema) {
   return arktypeValidator(target, schema, (result) => {
     if (result.success === false)
-      throw new DetailedError('Validation failed', { statusCode: 400, detail: result.errors })
+      throw new DetailedError('Validation failed', { statusCode: 400, detail: JSON.stringify(result.errors) })
   })
 }
