3defde6e1498e8ad238c10f99db1c14e047e77ed
NamesMT <dangquoctrung123@gmail.com>
2024-09-15 21:30:30 +0000

    chore: use `defineConfig` for intellisense

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,8 +1,15 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+
 import sharedConfig from './shared.config'
 
-export default {
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude!, 'shared.config.ts'],
+    },
+  },
   resolve: {
     alias: sharedConfig.alias,
   },
   ...sharedConfig,
-}
+})
